ea5aa3ceac361e3f479a6502cee1f23040a2727c
Fire Base <med.itrack199@gmail.com>
2025-08-29 16:38:40 +0000

    que repositorio deberia importar de github? hacemos uno con estos cambio

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBI96qh9EbuO0h8F1MBAYKwOloCrxaf6rk",
   authDomain: "med-itrack199.firebaseapp.com",
