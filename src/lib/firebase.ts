fd0a7a62fd26f90160a8dcdb9147627c7172a78b
Fire Base <med.itrack199@gmail.com>
2025-08-29 07:22:53 +0000

    si estoy bien logeado pero solo me sale este

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,12 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-    app = initializeApp(firebaseConfig);
-} else {
-    app = getApp();
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
 const storage = getStorage(app);
