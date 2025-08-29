a82129de960eda9f2b65aa7f5f0a1fddb864abbc
Fire Base <med.itrack199@gmail.com>
2025-08-29 07:38:00 +0000

    Extraction error: Error: [GoogleGenerativeAI Error]: Error fetching from

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genk-ai/googleai';
+import {config} from 'dotenv';
+
+config(); // Carga las variables de entorno desde el archivo .env
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiVersion: 'v1beta'})],
   model: 'googleai/gemini-1.5-flash-latest',
 });
