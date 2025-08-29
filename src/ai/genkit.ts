757ca7b8c1b8abf74b0bf60234c73cace50e2a46
Fire Base <med.itrack199@gmail.com>
2025-08-29 15:52:06 +0000

    Error: [GoogleGenerativeAI Error]: Error fetching from https://generativ

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -10,6 +10,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta'})],
+  plugins: [
+    googleAI({
+      apiVersion: 'v1beta',
+    }),
+  ],
   model: 'googleai/gemini-1.5-flash-latest',
 });
