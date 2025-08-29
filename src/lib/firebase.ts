5ea877eafa5c44bccc9a61037736a075311efb62
Fire Base <med.itrack199@gmail.com>
2025-08-29 15:18:32 +0000

    Med-iTrack Proyecto	med-itrack-470514 es que el proyecto en cloud es est

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    authDomain: "med-itrack-470514.firebaseapp.com",
+    projectId: "med-itrack-470514",
+    storageBucket: "med-itrack-470514.appspot.com",
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
