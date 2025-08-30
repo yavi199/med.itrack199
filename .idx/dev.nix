fbfb8d6d03faf928cd861e3ffee49d9fb50e5ae6
franyavier <franyavier@hotmail.com>
2025-08-30 23:33:20 +0000

    feat: Full deployment to Firebase App Hosting and Hosting

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,7 +5,7 @@
   channel = "stable-24.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs_22
     pkgs.zulu
   ];
   # Sets environment variables in the workspace
