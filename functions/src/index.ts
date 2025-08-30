50f96f4462ef69cf6713062c7be348d8a7e49628
Yavi <franyavier@hotmail.com>
2025-08-30 19:43:54 +0000

    ven pero si no puedo publicar como me dices que todo esta bien? igual fu

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -24,9 +24,9 @@ import * as logger from "firebase-functions/logger";
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({maxInstances: 10});
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
